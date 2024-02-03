27b62b4f80ad11b4bb1b1c7f4c9a2751123e0b14
Kamenchu <iankamenchu@gmail.com>
2024-02-03 10:40:30 +0300

    About component

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About(props) {
+    const defaultImageSrc = "https://via.placeholder.com/215";
+
+    return(
+        <aside>
+            <img src={props.imgsrc || defaultImageSrc} alt="blog logo" />
+            <p>{props.aboutText}</p>
+        </aside>
+    )
+}
+
+export default About;
